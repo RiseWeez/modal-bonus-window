346445bedded26ee03481c8cfe2ee0cfbcf48399
RiseWeez <115504828+RiseWeez@users.noreply.github.com>
2022-11-04 14:54:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-const span = document.getElementsByClassName("close")[0];
+// const span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+
+// Working function
+
+// function rangeSlide(value) {
+//     document.getElementById('rangeValue').innerHTML = value;
+// }
+
+
+const rangeInputs = document.querySelectorAll('input[type="range"]')
+const numberInput = document.querySelector('input[type="number"]')
+
+function handleInputChange(e) {
+  let target = e.target
+  if (e.target.type !== 'range') {
+    target = document.getElementById('range')
+  } 
+  const min = target.min
+  const max = target.max
+  const val = target.value
+  
+  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
 }
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
+rangeInputs.forEach(input => {
+  input.addEventListener('input', handleInputChange)
+})
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+numberInput.addEventListener('input', handleInputChange)
