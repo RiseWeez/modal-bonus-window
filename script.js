d6d9b15f708438ed296ba2f580b4241dd6b74378
RiseWeez <115504828+RiseWeez@users.noreply.github.com>
2022-11-06 23:07:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,30 +15,50 @@
 // }
 
 
-// Working function
+// Values changing upon slider moves
 
-// function rangeSlide(value) {
-//     document.getElementById('rangeValue').innerHTML = value;
-// }
+function rangeSlide(value) {
 
+    document.getElementById('rangeValue').innerHTML = value;
+    document.getElementById('rangeValue1').innerHTML = value;
+    document.getElementById('finalPrice').innerHTML = 10000 - value;
 
-const rangeInputs = document.querySelectorAll('input[type="range"]')
-const numberInput = document.querySelector('input[type="number"]')
-
-function handleInputChange(e) {
-  let target = e.target
-  if (e.target.type !== 'range') {
-    target = document.getElementById('range')
-  } 
-  const min = target.min
-  const max = target.max
-  const val = target.value
-  
-  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+    // console.log(value);
 }
 
-rangeInputs.forEach(input => {
-  input.addEventListener('input', handleInputChange)
+// Input range limit
+
+const input = document.querySelector("input");
+const sliderLimit = 1000;
+
+
+input.addEventListener("change", () => {
+  if (input.value > sliderLimit)
+    input.value = sliderLimit;
 })
 
-numberInput.addEventListener('input', handleInputChange)
+
+
+// Background 
+
+// const rangeInputs = document.querySelectorAll('input[type="range"]')
+// const numberInput = document.querySelector('input[type="number"]')
+
+// function handleInputChange(e) {
+//   let target = e.target
+//   if (e.target.type !== 'range') {
+//     target = document.getElementById('range')
+//   } 
+//   const min = target.min
+//   const max = target.max
+//   const val = target.value
+  
+//   target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+// }
+
+// rangeInputs.forEach(input => {
+//   input.addEventListener('input', handleInputChange)
+// })
+
+// numberInput.addEventListener('input', handleInputChange)
+
