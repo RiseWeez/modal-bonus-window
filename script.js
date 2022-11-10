71e3800bc63c76a830a0078d38c619de6b6d447d
RiseWeez <115504828+RiseWeez@users.noreply.github.com>
2022-11-10 17:54:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,79 +1,58 @@
-// const span = document.getElementsByClassName("close")[0];
-
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
 // Values changing upon slider moves
 
-const bonusValue = 6000;
+let bonusValue = 3000;
+let maxDiscount = 7999;
+let itemPrice = 8000;
 const currencyValue = bonusValue;
 
 function rangeSlide(value) {
-  document.getElementById('rangeValue').innerHTML = value;
-  document.getElementById('rangeValue1').innerHTML = value;
-  document.getElementById('finalPrice').innerHTML = 10000 - value;
+  const priceDiff = itemPrice - value
+  document.getElementById('selectedDiscount').innerHTML = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  document.getElementById('discountBonusValue').innerHTML = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  document.getElementById('finalPrice').innerHTML = priceDiff.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 
-  document.getElementById('rangeValue').style.color = '#00a93e';
-  document.getElementById('rangeValue1').style.color = '#00a93e';
+  document.getElementById('selectedDiscount').style.color = '#00a93e';
+  document.getElementById('discountBonusValue').style.color = '#00a93e';
 
   if (value > bonusValue) {
-    document.getElementById('rangeValue').style.color = 'red';
-    document.getElementById('rangeValue1').style.color = 'red';
+    document.getElementById('selectedDiscount').style.color = 'red';
+    document.getElementById('discountBonusValue').style.color = 'red';
   }
-
-  // console.log(value);
 }
 
 document.getElementById('bonusValue').innerHTML = bonusValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 document.getElementById('currencyValue').innerHTML = currencyValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+document.getElementById('maxDiscount').innerHTML = maxDiscount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+document.getElementById('itemPrice').innerHTML = itemPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 
 // Input range limit
 
-const input = document.querySelector('input');
+const input = document.querySelector('.range');
 
 input.addEventListener('change', () => {
   if (input.value > bonusValue) input.value = bonusValue;
 });
 
 // Background
-// const bonusValue = document.getElementById("bonusValue").innerText
 const target = document.querySelectorAll('input[type="range"]')[0];
 target.value = bonusValue;
 const min = target.min;
-const max = target.max;
-// const val = Number(bonusValue.split(' ').join(''))
+target.max = maxDiscount;
 
-target.style.backgroundSize = ((bonusValue - min) * 100) / (max - min) + '% 100%';
+target.style.backgroundSize = ((bonusValue - min) * 100) / (maxDiscount - min) + '% 100%';
 
-console.log(bonusValue);
+function changeItemPrice() {
+  const itemPriceInput = document.getElementById("itemPriceInput").value;
+  itemPrice = itemPriceInput
+}
 
-// const rangeInputs = document.querySelectorAll('input[type="range"]')
-// const numberInput = document.querySelector('input[type="number"]')
-// function handleInputChange(e) {
-//   let target = e.target
-//   if (e.target.type !== 'range') {
-//     target = document.getElementById('range')
-//   }
-//   const min = target.min
-//   const max = target.max
-//   const val = target.value
+function changeMaxDiscount() {
+  const maxDiscountInput = document.getElementById("maxDiscountInput").value;
+  maxDiscount = maxDiscountInput
+}
 
-//   target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
-// }
+function changeBonusValue() {
+  let bonusValueInput = document.getElementById("bonusValueInput").value;
+  bonusValue = bonusValueInput
+}
 
-// rangeInputs.forEach(input => {
-//   input.addEventListener('input', handleInputChange)
-// })
-
-// numberInput.addEventListener('input', handleInputChange)
