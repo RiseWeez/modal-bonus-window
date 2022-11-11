c4b8bc860383b4fc20e9c32a0e67c6f7aa54269a
RiseWeez <115504828+RiseWeez@users.noreply.github.com>
2022-11-11 17:57:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 // Values changing upon slider moves
 
-let bonusValue = 3000;
-let maxDiscount = 7999;
-let itemPrice = 8000;
-const currencyValue = bonusValue;
+let bonusValue = 2000;
+let maxDiscount = 9999;
+let itemPrice = 10000;
+let currencyValue = bonusValue;
+
+itemPriceInput.oninput = function changeItemPrice() {
+  let itemPriceInput = document.getElementById("itemPriceInput").value;
+  document.getElementById('itemPrice').innerHTML = itemPriceInput.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  console.log(isNaN(itemPriceInput));
+}
+
+maxDiscountInput.oninput = function changeMaxDiscount() {
+  let maxDiscountInput = document.getElementById("maxDiscountInput").value;
+  document.getElementById('maxDiscount').innerHTML = maxDiscountInput.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
+
+bonusValueInput.oninput = function changeBonusValue() {
+  let bonusValueInput = document.getElementById("bonusValueInput").value;
+  document.getElementById('bonusValue').innerHTML = bonusValueInput.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  bonusValue = currencyValue
+  console.log(isNaN(bonusValueInput));
+}
 
 function rangeSlide(value) {
   const priceDiff = itemPrice - value
@@ -18,12 +36,14 @@ function rangeSlide(value) {
     document.getElementById('selectedDiscount').style.color = 'red';
     document.getElementById('discountBonusValue').style.color = 'red';
   }
+  // console.log(typeof itemPriceInput);
 }
 
 document.getElementById('bonusValue').innerHTML = bonusValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 document.getElementById('currencyValue').innerHTML = currencyValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 document.getElementById('maxDiscount').innerHTML = maxDiscount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 document.getElementById('itemPrice').innerHTML = itemPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+
 
 // Input range limit
 
@@ -41,18 +61,8 @@ target.max = maxDiscount;
 
 target.style.backgroundSize = ((bonusValue - min) * 100) / (maxDiscount - min) + '% 100%';
 
-function changeItemPrice() {
-  const itemPriceInput = document.getElementById("itemPriceInput").value;
-  itemPrice = itemPriceInput
-}
 
-function changeMaxDiscount() {
-  const maxDiscountInput = document.getElementById("maxDiscountInput").value;
-  maxDiscount = maxDiscountInput
-}
 
-function changeBonusValue() {
-  let bonusValueInput = document.getElementById("bonusValueInput").value;
-  bonusValue = bonusValueInput
-}
+
+
 
